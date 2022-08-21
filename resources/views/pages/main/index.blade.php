46e8d3f8bf42c3c26714fb504ecc49acbef7cc8f
ajayjury <ajay@jurysoft.com>
2022-08-22 01:41:21 +0530

    home page changes

--- resources/views/pages/main/index.blade.php
+++ resources/views/pages/main/index.blade.php
@@ -3,7 +3,7 @@
 @section('css')
 <style>
 .about-tai-content img{
-    width: 40%;
+    height:235px;
     object-fit: contain;
     margin: 0 15px 15px 15px;
 }
@@ -18,6 +18,7 @@
     .float-left{ float: none !important; }
     .float-right{ float: none !important; }
     .about-tai-content img{
+        height:auto;
         width: 100%;
         object-fit: contain;
         margin: 0px;
@@ -48,7 +49,7 @@
 <!-- ======== Hero Area End ========== -->
 
 <!-- ======== Church About Area Start ========== -->
-<div class="church-about-area  section-space--pt_120">
+<div class="church-about-area  section-space--pt_120  section-space--pb_90">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
@@ -56,11 +57,26 @@
                     <img src="{{ asset('main/images/hero/banner7.jpg')}}" class="img-fluid float-left" alt="About Images">
                     <div class="section-title-wrap" style="display:inline">
                         <h3 class="section-title--two  left-style mb-30">What is Amrita Janani?</h3>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised</p>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised</p>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised</p>
+                        <p>Amrita Janani is an online digital knowledge repository containing the teachings of Guruji Sri Amritananda Natha Saraswati of Devipuram, Vizag, India. The teachings are in the form of lectures, practice manuals, guided meditations, etc.</p>
+                        <a href="" class="submit-btn">Learn More</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="church-about-area  section-space--pb_90">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="about-tai-content">
+                    <div class="section-title-wrap" style="display:inline">
+                        <h3 class="section-title--two  left-style mb-30">How To Access Amrita Janani contents?</h3>
+                        <p>To acces the contents, create a new account by clicking the Login/Signup button and entering your details along with a unique password in the "Register" section.</p>
+                        <p>Click the Register button. An OTP will be sent to the registered email ID. Enter the OTP in the Verify Email page to activate the account.</p>
+                        <p>Access Amrita Janani contents by logging in with the registered email ID and password in the "Login" section.</p>
                     </div>
                 </div>
 
@@ -71,19 +87,18 @@
 <!-- ======== Church About Area End ========== -->
 
 <!-- ======== Hindu Video Area Start ========== -->
-<div class="hindu-video-area  section-space--pt_90  section-space--pb_120">
+<div class="hindu-video-area  section-space--pb_120">
     <div class="container">
         <div class="hindu-video-bg hindu-video-section-pb bg-overlay-black border-radius-5">
             <div class="row">
                 <div class="col-lg-8 ml-auto mr-auto">
                     <div class="video-content-wrap text-center">
                         <div class="icon">
-                            <a href="https://www.youtube.com/watch?v=d7jAiAYusUg" class="video-link popup-youtube"><img src="{{ asset('main/images/icons/play-circle.png')}}" alt="Video Icon"></a>
+                            <a href="https://www.youtube.com/watch?v=5UWwNpilnz0" class="video-link popup-youtube"><img src="{{ asset('main/images/icons/play-circle.png')}}" alt="Video Icon"></a>
                         </div>
                         <div class="content section-space--mt_80">
-                            <h3 class="text-white mb-10">Best speech on religious life</h3>
-                            <p class="text-white">On the other hand, we denounce with righteous indignation and dislike men who are so
-                                beguiled and demoralized by the charms of pleasure of the moment</p>
+                            <h3 class="text-white mb-10">Who is Guruji Sri Amritananda Natha Saraswati</h3>
+                            <p class="text-white">Learn more about Guruji Sri Amritananda Natha Saraswati from a part of the documentary on Guruji by renowned documentary filmmaker Raja Choudhury.</p>
                         </div>
                     </div>
                 </div>
