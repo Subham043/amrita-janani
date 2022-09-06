95cc3d1ba8ead9fd7e91f7ec2c86dc933a2bc325
ajayjury <ajay@jurysoft.com>
2022-09-06 17:46:45 +0530

    completed dashboard

--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -13,12 +13,12 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                    <h4 class="mb-sm-0">Projects</h4>
+                                    <h4 class="mb-sm-0">Amrita Janani</h4>
 
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
                                             <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboards</a></li>
-                                            <li class="breadcrumb-item active">Projects</li>
+                                            <li class="breadcrumb-item active">Amrita Janani</li>
                                         </ol>
                                     </div>
 
